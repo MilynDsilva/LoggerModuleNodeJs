f3e625982edd8590ba2b5009af34e218bd535958
Milyn <milyg.dsilva@gmail.com>
2022-01-01 16:49:23 +0530

    commit

---
+++ newapp.js
@@ -0,0 +1,14 @@
+//const newlogger = require('./newlogger'); //same folder
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+
+
+const Logger = require('./newlogger');
+const logger = new Logger();
+//reg emitter
+emitter.on('messgaelogged',function(arg){
+    console.log('listner called',arg);
+});
+
+logger.log('message-s');
--- newlogger.js
+++ newlogger.js
@@ -1,12 +1,17 @@
 const EventEmitter = require('events'); //class
-const emitter = new EventEmitter();//object
+// const emitter = new EventEmitter();//object
 
 var url = 'http://mylogger.io/log';
 
-function log(message){
+class Logger extends EventEmitter{
+
+log(message){
     //send http message
     console.log(message);
-}
+    
+    //raise an event
+    this.emit('messagelogged',{id:1,url:'http://someurl'});
+}}
 
-module.exports = log;
+module.exports = Logger;
 //module.exports.endurl = url;
